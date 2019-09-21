f49cd0757cbaa959582c8b03f88f219a7d09443e
estev97 <estev97@gmail.com>
2019-09-21 01:41:34 -0600

    Pjct_Final

---
+++ precache-manifest.ada07a7059c67c1fb4e622fbcab369b8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "f242238c1ac5dd21aace",
+    "url": "/js/chunk-vendors.c161960c.js"
+  },
+  {
+    "revision": "79fbdd62ebeaabcee1dd",
+    "url": "/js/app.26735081.js"
+  },
+  {
+    "revision": "788fb1ab28a43d469bdab24e8b754e93",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9762bb26ed73a898b5031703bdf6f91a",
+    "url": "/img/clavala.9762bb26.svg"
+  },
+  {
+    "revision": "d1fb5bbc4e5620643e99f493dcf0fdd0",
+    "url": "/img/21501188.d1fb5bbc.jpg"
+  },
+  {
+    "revision": "bfa864de4dad0a4c56661377e03b7d2b",
+    "url": "/img/2147878.bfa864de.jpg"
+  },
+  {
+    "revision": "f242238c1ac5dd21aace",
+    "url": "/css/chunk-vendors.87cf3782.css"
+  },
+  {
+    "revision": "79fbdd62ebeaabcee1dd",
+    "url": "/css/app.d07e4c55.css"
+  }
+];
